db31d818aee8f48eb679a52426e2e2f6e86946bd
lexives <lexives96@gmail.com>
2021-10-14 10:12:11 -0500

    initial commit

---
+++ app/src/main/java/com/delarax/icewindDale/companion/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.delarax.icewindDale.companion
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.runtime.Composable
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // TODO
+//        setContent {
+//
+//        }
+    }
+}
