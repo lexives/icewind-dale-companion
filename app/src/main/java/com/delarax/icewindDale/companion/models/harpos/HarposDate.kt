1e5c928ac3e845050109c584bdaa833902906a96
lexives <lexives96@gmail.com>
2021-10-15 14:04:55 -0500

    add shortcut methods to get holidays

--- app/src/main/java/com/delarax/icewindDale/companion/models/harpos/HarposDate.kt
+++ app/src/main/java/com/delarax/icewindDale/companion/models/harpos/HarposDate.kt
@@ -1,5 +1,6 @@
 package com.delarax.icewindDale.companion.models.harpos
 
+import com.delarax.icewindDale.companion.models.InvalidDateException
 import com.delarax.icewindDale.companion.models.harpos.HarposMonth.*
 
 data class HarposDate(
@@ -7,7 +8,29 @@ data class HarposDate(
     val month: HarposMonth?,
     val year: Int,
     val holiday: HarposHoliday? = null
-)
+) {
+    companion object {
+        @Throws(InvalidDateException::class)
+        fun midwinter(year: Int) : HarposDate = getHoliday(HarposHoliday.MIDWINTER, year)
+        @Throws(InvalidDateException::class)
+        fun shieldmeet(year: Int) : HarposDate = getHoliday(HarposHoliday.SHIELDMEET, year)
+        @Throws(InvalidDateException::class)
+        fun greengrass(year: Int) : HarposDate = getHoliday(HarposHoliday.GREENGRASS, year)
+        @Throws(InvalidDateException::class)
+        fun midsummer(year: Int) : HarposDate = getHoliday(HarposHoliday.MIDSUMMER, year)
+        @Throws(InvalidDateException::class)
+        fun highharvesttide(year: Int) : HarposDate = getHoliday(HarposHoliday.HIGHHARVESTTIDE, year)
+        @Throws(InvalidDateException::class)
+        fun moonFeast(year: Int) : HarposDate = getHoliday(HarposHoliday.MOON_FEAST, year)
+
+        @Throws(InvalidDateException::class)
+        private fun getHoliday(holiday: HarposHoliday, year: Int) : HarposDate {
+            val date = HarposDate(day = 1, month = null, year = year, holiday = holiday)
+            if (!date.isValid()) { throw(InvalidDateException(date)) }
+            return date
+        }
+    }
+}
 
 enum class HarposMonth(val num: Int, val commonName: String) {
     HAMMER(1, "Deepwinter"),
--- app/src/main/java/com/delarax/icewindDale/companion/models/nunavut/NunavutDate.kt
+++ app/src/main/java/com/delarax/icewindDale/companion/models/nunavut/NunavutDate.kt
@@ -1,5 +1,6 @@
 package com.delarax.icewindDale.companion.models.nunavut
 
+import com.delarax.icewindDale.companion.models.InvalidDateException
 import com.delarax.icewindDale.companion.models.nunavut.NunavutAbbreviation.*
 import com.delarax.icewindDale.companion.models.nunavut.NunavutSeason.*
 
@@ -8,7 +9,27 @@ data class NunavutDate(
     val season: NunavutSeason?,
     val year: Int,
     val holiday: NunavutHoliday? = null
-)
+) {
+    companion object {
+        @Throws(InvalidDateException::class)
+        fun midwinter(year: Int) : NunavutDate = getHoliday(NunavutHoliday.MIDWINTER, year)
+        @Throws(InvalidDateException::class)
+        fun omingmak(year: Int) : NunavutDate = getHoliday(NunavutHoliday.OMINGMAK, year)
+        @Throws(InvalidDateException::class)
+        fun sunFestival(year: Int) : NunavutDate = getHoliday(NunavutHoliday.SUN_FESTIVAL, year)
+        @Throws(InvalidDateException::class)
+        fun alianat(year: Int) : NunavutDate = getHoliday(NunavutHoliday.ALIANAT, year)
+        @Throws(InvalidDateException::class)
+        fun moonFeast(year: Int) : NunavutDate = getHoliday(NunavutHoliday.MOON_FEAST, year)
+
+        @Throws(InvalidDateException::class)
+        private fun getHoliday(holiday: NunavutHoliday, year: Int) : NunavutDate {
+            val date = NunavutDate(day = 1, season = null, year = year, holiday = holiday)
+            if (!date.isValid()) { throw(InvalidDateException(date)) }
+            return date
+        }
+    }
+}
 
 enum class NunavutSeason(
     val num: Int,
